52664d37cd6a0988bd6b8edc43811caf45a826d0
Vanessa Beatriz González Beltrame <167648640+VanessaBeatriz@users.noreply.github.com>
2024-08-09 22:03:42 -0300

    Add files via upload

---
+++ exercicio.1c.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+	int num;
+	printf("Digite um numero: ");
+	scanf("%d",&num);
+	if(num % 5 == 0){
+		printf("O numero %d e div por 5.");	
+	}else{
+	printf("O numero %d e nao div por 5.");
+	}
+	
+	return 0;
+}
